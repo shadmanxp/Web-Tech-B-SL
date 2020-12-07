1004159f9a4def1b56248846b8e550e5a29a4316
shadmanxp <57424458+shadmanxp@users.noreply.github.com>
2020-12-07 21:22:33 +0600

    Add files via upload

---
+++ Lab Task 7/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="mycss.css" crossorigin="anonymous">
+
+	
+</head>
+<body class="stretchview">
+	<div class="header">
+		<div class="container">
+			<div class="row ">
+				<div>
+			     <img src="logo5.png" alt="logo" width="60" height="60">
+			    </div>
+			    <div>
+			     <h1 class="headerh1">FARM FOOD</h1>
+			    </div>
+			    <div class="col-4">
+			    </div>
+			    <div class="col-sm-auto headerpadding">
+			     	<form class="form-inline">
+	    				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+	    				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+ 					 </form>
+			    </div>
+			    <div class="headerpadding headerdropdown col-sm-auto">
+				    <div class="btn-groups">
+				     	<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">REGISTER/LOGIN</button>
+					     <div class="dropdown-menu">
+						    <a class="dropdown-item " href="#">REGISTER</a>
+						    <a class="dropdown-item " href="#">LOGIN</a>
+					  	</div>
+				    </div>
+				</div>
+				<div class="col-sm-auto headerpadding headerdisplaynone">
+			    	<button type="button" class="btn btn-success">Login</button>
+			    </div>
+			    <div class="col-sm-auto headerpadding headerdisplaynone">
+			      	<button type="button" class="btn btn-success">Register</button>
+			    </div>
+			</div>
+	  	</div>
+	</div>
+</body>
+</html>
---
+++ Lab Task 7/homepage.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+	<?php include("header.php")?>
+	<div class="divider"><p>Fresh farm food at your doorstep</p></div>
+	<div class="container bodytrim">
+		<div class="row ">
+			<div class="col-md-auto hidelargenav">
+			    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+			      <a class="nav-link  active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+			      <a class="nav-link " id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">FRUITS AND VEGETABLES</a>
+			      <a class="nav-link " id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">MEAT AND FISH</a>
+			      <a class="nav-link " id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">SPICE AND GRAINS</a>
+			    </div>
+			</div>
+			<div class="col-sm-auto hidesmallnav">
+			    <div class="nav nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+			      <a class="nav-link  active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+			      <a class="nav-link " id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">FRUITS AND VEGETABLES</a>
+			      <a class="nav-link " id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">MEAT AND FISH</a>
+			      <a class="nav-link " id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">SPICE AND GRAINS</a>
+			    </div>
+			</div>
+
+			<div class="col-sm">
+				<div class="tab-content" id="v-pills-tabContent">
+				    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+				    	<div class="row">
+				    		<div class="col-sm ">
+					    		<div id="carouselExampleFade " class="carousel slide carousel-fade " data-ride="carousel">
+			  						<div class="carousel-inner shadow-lg mb-5 bg-white rounded ">
+				    					<div class="carousel-item active">
+				      						<img src="fruitsandvegetables.jpg" class="d-block w-100" width="200" height="" alt="fruitsandvegetables.jpg">
+				    					</div>
+				    					<div class="carousel-item">
+									     	<img src="meatandfish.jpg" class="d-block w-100" width="200" height="" alt="meatandfish.jpg">
+									    </div>
+				    					<div class="carousel-item">
+									        <img src="spiceandgrains.jpeg" class="d-block w-100" width="200" height="" alt="spiceandgrains.jpeg">
+									    </div>
+			  						</div>
+									<a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a>
+									<a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>
+								</div>
+				    		</div>
+				    	</div>
+				    </div>
+				    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"></div>
+				    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+				    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+			    </div>
+			    
+			</div>
+		</div>
+	</div>
+	<?php include("footer.php")?>
+</body>
+</html>
